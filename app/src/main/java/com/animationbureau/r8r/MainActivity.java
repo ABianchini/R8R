2e81a4e3656c32adccf22fe0bdad5988cb75d380
AdrianoLaptop <animationbureau@gmail.com>
2016-06-06 12:28:02 -0700

    Saved always

--- app/src/main/java/com/animationbureau/r8r/MainActivity.java
+++ app/src/main/java/com/animationbureau/r8r/MainActivity.java
@@ -1,14 +1,15 @@
 package com.animationbureau.r8r;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
-import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.HorizontalScrollView;
 import android.widget.TextView;
 
@@ -30,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     TextView negThreeText;
     TextView negFourText;
     TextView negFiveText;
+    EditText whatEdit;
+    EditText whyEdit;
     HorizontalScrollView scrollRater;
 
     @Override
@@ -53,8 +56,9 @@ public class MainActivity extends AppCompatActivity {
         negFourText = (TextView) findViewById(R.id.textViewNegFour);
         negFiveText = (TextView) findViewById(R.id.textViewNegFive);
 
+        whatEdit = (EditText) findViewById(R.id.editTextWhat);
+        whyEdit = (EditText) findViewById(R.id.editTextWhy);
         scrollRater = (HorizontalScrollView) findViewById(R.id.horizontalScrollView);
-
 
         Resources r = getResources();
         float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16, r.getDisplayMetrics());
@@ -66,13 +70,58 @@ public class MainActivity extends AppCompatActivity {
         fiveText.setPadding((int)px,0,fiveWidth,0);
         scrollRater.setFadingEdgeLength((int)px*8);
 
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        whatS = sharedPref.getString("whatStored", "");
+        whyS = sharedPref.getString("whyStored","");
+        r8ing = sharedPref.getInt("r8ingStored",0);
+        whatEdit.setText(whatS);
+        whyEdit.setText(whyS);
+
         Handler h = new Handler();
         h.postDelayed(new Runnable() {
             @Override
             public void run() {
-                scrollRater.smoothScrollTo(zeroText.getLeft() + (zeroText.getWidth() - width)/2,0);
+                int x = zeroText.getLeft() + (zeroText.getWidth() - width)/2;
+                switch (r8ing) {
+                    case -5:    x = negFiveText.getLeft() + (negFiveText.getWidth() - width)/2;
+                        break;
+                    case -4:    x = negFourText.getLeft() + (negFourText.getWidth() - width)/2;
+                        break;
+                    case -3:    x = negThreeText.getLeft() + (negThreeText.getWidth() - width)/2;
+                        break;
+                    case -2:    x = negTwoText.getLeft() + (negTwoText.getWidth() - width)/2;
+                        break;
+                    case -1:    x = negOneText.getLeft() + (negOneText.getWidth() - width)/2;
+                        break;
+                    case 0:     x = zeroText.getLeft() + (zeroText.getWidth() - width)/2;
+                        break;
+                    case 1:     x = oneText.getLeft() + (oneText.getWidth() - width)/2;
+                        break;
+                    case 2:     x = twoText.getLeft() + (twoText.getWidth() - width)/2;
+                        break;
+                    case 3:     x = threeText.getLeft() + (threeText.getWidth() - width)/2;
+                        break;
+                    case 4:     x = fourText.getLeft() + (fourText.getWidth() - width)/2;
+                        break;
+                    case 5:     x = fiveText.getLeft() + (fiveText.getWidth() - width)/2;
+                        break;
+                }
+                scrollRater.smoothScrollTo(x,0);
             }
         },500);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SharedPreferences sharedPref =  this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        whatS = whatEdit.getText().toString();
+        whyS = whyEdit.getText().toString();
+        editor.putString("whatStored",whatS);
+        editor.putString("whyStored",whyS);
+        editor.putInt("r8ingStored",r8ing);
+        editor.apply();
     }
 
     @Override
@@ -112,58 +161,73 @@ public class MainActivity extends AppCompatActivity {
         scrollRater.smoothScrollTo(x,0);
     }
 
+    public void clickClearAll(View view) {
+        //TODO here work!
+    }
+
     public void clickFive(View view) {
         int x = fiveText.getLeft() + (fiveText.getWidth() - width)/2;
         scrollRater.smoothScrollTo(x,0);
+        r8ing = 5;
     }
 
     public void clickFour(View view) {
         int x = fourText.getLeft() + (fourText.getWidth() - width)/2;
         scrollRater.smoothScrollTo(x,0);
+        r8ing = 4;
     }
 
     public void clickThree(View view) {
         int x = threeText.getLeft() + (threeText.getWidth() - width)/2;
         scrollRater.smoothScrollTo(x,0);
+        r8ing = 3;
     }
 
     public void clickTwo(View view) {
         int x = twoText.getLeft() + (twoText.getWidth() - width)/2;
         scrollRater.smoothScrollTo(x,0);
+        r8ing = 2;
     }
 
     public void clickOne(View view) {
         int x = oneText.getLeft() + (oneText.getWidth() - width)/2;
         scrollRater.smoothScrollTo(x,0);
+        r8ing = 1;
     }
 
     public void clickZero(View view) {
         int x = zeroText.getLeft() + (zeroText.getWidth() - width)/2;
         scrollRater.smoothScrollTo(x,0);
+        r8ing = 0;
     }
 
     public void clickNegOne(View view) {
         int x = negOneText.getLeft() + (negOneText.getWidth() - width)/2;
         scrollRater.smoothScrollTo(x,0);
+        r8ing = -1;
     }
 
     public void clickNegTwo(View view) {
         int x = negTwoText.getLeft() + (negTwoText.getWidth() - width)/2;
         scrollRater.smoothScrollTo(x,0);
+        r8ing = -2;
     }
 
     public void clickNegThree(View view) {
         int x = negThreeText.getLeft() + (negThreeText.getWidth() - width)/2;
         scrollRater.smoothScrollTo(x,0);
+        r8ing = -3;
     }
 
     public void clickNegFour(View view) {
         int x = negFourText.getLeft() + (negFourText.getWidth() - width)/2;
         scrollRater.smoothScrollTo(x,0);
+        r8ing = -4;
     }
 
     public void clickNegFive(View view) {
         int x = negFiveText.getLeft() + (negFiveText.getWidth() - width)/2;
         scrollRater.smoothScrollTo(x,0);
+        r8ing = -5;
     }
 }
