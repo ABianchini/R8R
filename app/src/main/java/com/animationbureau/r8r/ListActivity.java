a40c51f9056e45a1794694ac01bb8613e73a7ae8
animationb <animationbureau@gmail.com>
2016-06-08 10:06:11 -0700

    Fixed list and organized TODO's

--- app/src/main/java/com/animationbureau/r8r/ListActivity.java
+++ app/src/main/java/com/animationbureau/r8r/ListActivity.java
@@ -2,6 +2,9 @@ package com.animationbureau.r8r;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -10,8 +13,38 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
+        ArrayList<R8s> r8sList = new ArrayList<R8s>();
 
+        R8s r8s1 = new R8s();
+        r8s1.setID(1);
+        r8s1.setWhat("Harry Potter Ride");
+        r8s1.setr8(-5);
+        r8s1.setWhy("Tween line simulator 2k16");
+        r8sList.add(r8s1);
+        R8s r8s2 = new R8s();
+        r8s2.setID(2);
+        r8s2.setWhat("This App");
+        r8s2.setr8(-2);
+        r8s2.setWhy("I can't take selfies with it.");
+        r8sList.add(r8s2);
 
+        r8sAdapter adapter = new r8sAdapter(this,r8sList);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+        //TODO on click of list item
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //TODO create "delete all for database" and "reload list"
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/animationbureau/r8r/r8sAdapter.java
@@ -0,0 +1,35 @@
+package com.animationbureau.r8r;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by anima on 6/8/2016.
+ */
+public class r8sAdapter extends ArrayAdapter<R8s> {
+    public r8sAdapter(Context context, ArrayList<R8s> r8s) {
+        super(context, 0, r8s);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        R8s r8s = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_r8s,parent,false);
+        }
+        TextView tvWhat = (TextView) convertView.findViewById(R.id.tvWhat);
+        TextView tvR8 = (TextView) convertView.findViewById(R.id.tvR8);
+
+        tvWhat.setText(r8s.getWhat());
+        tvR8.setText(String.valueOf(r8s.getr8()));
+
+        return convertView;
+    }
+}
